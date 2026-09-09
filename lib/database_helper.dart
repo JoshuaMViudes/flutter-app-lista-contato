950dc53d3664b4782fd79aa05f11dc7f9e4a3e73
JoshuaMViudes <contadojosh2@gmail.com>
2026-09-09 16:36:22 -0300

    adicionar contato

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,6 +4,7 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseHelper {
   static Database? _db;
 
+  //Abre(ou cria, se não existe) o arquivo banco de dados
   static Future<Database> abrirBanco() async {
     final caminho = join(await getDatabasesPath(), 'contatos.db');
 
@@ -24,9 +25,31 @@ class DatabaseHelper {
     );
   }
 
+  // Getter que devolve o banco de dados já aberto, ou abre se ainda não existe
   static Future<Database> get database async {
     _db ??= await abrirBanco();
 
     return _db!;
+  }
+
+  // READ: Buscar todos as contatos salvos no banco
+  static Future<List<Map<String, dynamic>>> buscarContatos() async {
+    final db = await DatabaseHelper.database;
+    return db.query('contatos'); //SELECT * FROM contatos
+  }
+
+  //CREATE: Inserir um novo contato no banco de dados
+  static Future<void> inserirContato(
+    String inicial,
+    String nome,
+    String numero,
+  ) async {
+    final db = await DatabaseHelper.database;
+    await db.insert('contatos', {
+      'inicial': inicial,
+      'nome': nome,
+      'numero': numero,
+      'situacao': 0,
+    });
   }
 }
--- lib/lista_contatos_page.dart
+++ lib/lista_contatos_page.dart
@@ -1,78 +1,130 @@
+import 'package:app_listacontatos/database_helper.dart';
 import 'package:flutter/material.dart';
 
-class ListaContatosPage extends StatelessWidget {
+class ListaContatosPage extends StatefulWidget {
   const ListaContatosPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> contatos = [
-      {
-        "inicial": "AS",
-        "nome": "Ana Souza",
-        "numero": "(11) 98765-4321",
-        "situacao": true,
-      },
-      {
-        "inicial": "BL",
-        "nome": "Bruno Lima",
-        "numero": "(14) 99123-4567",
-        "situacao": false,
-      },
-      {
-        "inicial": "CM",
-        "nome": "Carla Mendes",
-        "numero": "(21) 97654-3210",
-        "situacao": true,
-      },
-      {
-        "inicial": "DA",
-        "nome": "Diego Alves",
-        "numero": "(19) 98888-1234",
-        "situacao": false,
-      },
-      {
-        "inicial": "ET",
-        "nome": "Elisa Torres",
-        "numero": "(17) 99999-5678",
-        "situacao": false,
-      },
-    ];
+  State<ListaContatosPage> createState() => _ListaContatosPageState();
+}
 
+class _ListaContatosPageState extends State<ListaContatosPage> {
+  List<Map<String, dynamic>> contatos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    carregarContatos();
+  }
+
+  void carregarContatos() async {
+    final dados = await DatabaseHelper.buscarContatos();
+    setState(() {
+      contatos = dados;
+    });
+  }
+
+  void adicionarContato() {
+    final inicialContatoController = TextEditingController();
+    final nomeContatoController = TextEditingController();
+    final numeroContatoController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Novo Contato'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: inicialContatoController,
+                decoration: InputDecoration(hintText: 'Digite a sigla...'),
+              ),
+              TextField(
+                controller: nomeContatoController,
+                decoration: InputDecoration(hintText: 'Digite o nome...'),
+              ),
+              TextField(
+                controller: numeroContatoController,
+                decoration: InputDecoration(hintText: 'Digite o número...'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                //Função para fechar qualquer janela/tela
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (nomeContatoController.text.isNotEmpty &&
+                    numeroContatoController.text.isNotEmpty &&
+                    inicialContatoController.text.isNotEmpty) {
+                  await DatabaseHelper.inserirContato(
+                    inicialContatoController.text,
+                    nomeContatoController.text,
+                    numeroContatoController.text,
+                  );
+                  carregarContatos();
+
+                  if (!context.mounted) return;
+
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meus Contatos"),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 15, 117, 168),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(12),
-        itemCount: contatos.length,
-        itemBuilder: (context, index) {
-          final contato = contatos[index];
-          final bool situacao = contato['situacao'];
+      body: contatos.isEmpty
+          ? Center(
+              child: Text('Nenhum contato ainda. Toque em + para adicionar'),
+            )
+          : ListView.builder(
+              padding: EdgeInsets.all(12),
+              itemCount: contatos.length,
+              itemBuilder: (context, index) {
+                final contato = contatos[index];
+                final bool situacao = contato['situacao'] == 1;
 
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.green,
-                child: Text(contato['inicial']),
-              ),
-              title: Text(
-                contato['nome'],
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(contato['numero']),
-              trailing: Icon(
-                situacao ? Icons.star : Icons.star,
-                color: situacao ? Colors.orange : Colors.grey,
-              ),
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.green,
+                      child: Text(contato['inicial']),
+                    ),
+                    title: Text(
+                      contato['nome'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(contato['numero']),
+                    trailing: Icon(
+                      situacao ? Icons.star : Icons.star,
+                      color: situacao ? Colors.orange : Colors.grey,
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => adicionarContato(),
         backgroundColor: Colors.orange,
         foregroundColor: Colors.white,
         shape: CircleBorder(),
